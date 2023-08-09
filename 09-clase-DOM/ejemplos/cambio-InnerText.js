b9f56a54ce3d1363fb993ac5a3c863b2b1d7b1fa
Fritz <fritz.developer10@gmail.com>
2023-08-09 18:22:09 -0500

    rename

--- 09-clase-DOM/ejemplos/cambio-InnerText.js
+++ 09-clase-DOM/ejemplos/cambio-InnerText.js
@@ -1,6 +1,7 @@
+//mostramos el contenido de texto con js en la consola
 let titulo = document.getElementById("titulo");
 console.log( titulo.innerText ); //"Hola mundo"
 
-// cambio de contenido texto con js
+// cambio de contenido texto con js en la consola y en la vista para el usuario
 titulo.innerText = "Hola Cofer!!!";
 console.log(titulo.innerText);// "Hola Coder"
