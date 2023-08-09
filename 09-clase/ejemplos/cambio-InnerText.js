02dd404dc769863a600368cd2b4c4240f42a5ee7
Fritz <fritz.developer10@gmail.com>
2023-08-09 18:03:48 -0500

    cambio de texto con js

---
+++ 09-clase/ejemplos/cambio-InnerText.js
@@ -0,0 +1,6 @@
+let titulo = document.getElementById("titulo");
+console.log( titulo.innerText ); //"Hola mundo"
+
+// cambio de contenido texto con js
+titulo.innerText = "Hola Cofer!!!";
+console.log(titulo.innerText);// "Hola Coder"
