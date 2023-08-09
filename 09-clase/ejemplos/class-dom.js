2541eda5cd2ff194004bf7f5432b0c6a599316f5
Fritz <fritz.developer10@gmail.com>
2023-08-08 23:44:03 -0500

    dom-9

---
+++ 09-clase/ejemplos/class-dom.js
@@ -0,0 +1,5 @@
+let paises  = document.getElementsByClassName("paises");
+
+console.log(paises[0].innerHTML); // AR
+console.log(paises[1].innerHTML); // PER
+console.log(paises[2].innerHTML); // UY
---
+++ 09-clase/ejemplos/id-dom.js
@@ -0,0 +1,5 @@
+let div = document.getElementById("app");
+let parrafo = document.getElementById("parrafo");
+
+console.log(div.innerHTML);
+console.log(parrafo.innerHTML);
