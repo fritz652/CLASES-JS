c7547d3de2e823b67c54c19547bbf76c24e4bc06
Fritz <fritz.developer10@gmail.com>
2023-08-08 22:24:30 -0500

    07

--- 07-clase/ejemplos/07-sort.js
+++ 07-clase/ejemplos/07-sort.js
@@ -7,6 +7,29 @@ console.log(numeros); // [1, 5, 40, 200]
 
 
 const numeros2 = [40, 1, 5,200];
-numeros.sort((a,b) => b - a); //Ordena el array en forma descendente
+numeros2.sort((a,b) => b - a); //Ordena el array en forma descendente
 console.log(numeros2); // [200, 40, 5, 1]
 //NOTA:  Es un método destructivo
+
+
+/* ::: Ejemplo 2 ::: */
+
+const numbers = [7,9,5,3,2,1];
+
+numbers.sort(
+    function (a,b){
+        return a-b;
+    }
+)
+
+console.log(numbers); // [1, 2, 3, 5, 7, 9]
+
+
+/* ::::Ejemplo3::::: */
+// Aqui veremos un ejemplo con strings
+
+const nombres = ["martin", "brian", "sebas", "leo", "camilo"];
+
+nombres.sort();
+
+console.log(nombres);// [ 'briam', 'camilo', 'leo', 'martin', 'sebas']
